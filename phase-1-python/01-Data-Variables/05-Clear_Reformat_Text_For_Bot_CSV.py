bf0fd4424c1d851228dc7b95c04a3566dc87391a
Ares007700 <aresforwork007700@gmail.com>
2026-05-20 03:22:33 +0600

    Re-Format, split, strip, join, lower, upper, CSV Data convert

---
+++ phase-1-python/01-Data-Variables/05-Clear_Reformat_Text_For_Bot_CSV.py
@@ -0,0 +1,24 @@
+Complaint_Msg = input("Please provide your complaint  ")
+Clean = Complaint_Msg.lower()
+print(Clean)
+
+#Complaint_Msg = input("Please provide your complaint  ").lower()  can be added. then writing another place won't be necessary for this work.
+
+#print(variable[0]) -> print the first string/ word. python counts 0 1 2 3....
+
+#variable.lower() -> NEED THIS = need this
+
+#len(variable) -> need this = 9   counts the character length including spaces
+
+#variable.upper() ->  need this = NEED THIS
+
+#Variable.split() -> I need this = ['I', 'need', 'this']  --- used in CSV Data files
+
+#Variable.split(',') -> I need this = ['I need this'] / I,need,this = ['I', 'need', 'this'] / I,     need,   this = ['I', '     need', '   this']
+
+#" ".join(Variable.strip()) -> output = o u t p u t  for the space in between quotation
+
+#" ".join(Variable.split()) -> makes I      need   this = I need this
+
+#"".join(Variable.split()) -> makes I      need   this = Ineedthis
+
