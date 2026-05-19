f2361baa4b9ea376fd0ec60b7bbf227f53844ae7
Ares007700 <aresforwork007700@gmail.com>
2026-05-20 03:19:08 +0600

    Use of print f to add multiple variable

---
+++ phase-1-python/01-Data-Variables/01-Product_Shipment_Confirm.py
@@ -0,0 +1,6 @@
+Name = input("Please enter your name  ")
+Product_ID = input("Please provide product ID  ")
+print(f"Hello {Name}, your product order {Product_ID} is shipped")   
+
+#f is used to write variables directly in print with a string
+#it can save time to add multiple variable
