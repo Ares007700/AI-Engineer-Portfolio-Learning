e73a05eb0e2acd1a7bb2d4aeb9dcbe73765d0c5e
Ares00700 <aresforwork007700@gmail.com>
2026-07-10 22:11:36 +0600

    Lesson

--- tool-1-fast-api/L_1_fastapi.py
+++ tool-1-fast-api/L_1_fastapi.py
@@ -29,7 +29,16 @@ def input(a: int, b: int):
 #return: Sends data back to the user
 #{"message": "Hello world"}: A Python dictionary. FastAPI automatically converts this into JSON format for the response
 
+#app.post: Listens for POST requests (standard for sending data to the server).
+#("/add"): The URL path for this endpoint.
+#add: Name of the function.
+#a: int: Defines a parameter named a. The : int part is type hinting. FastAPI uses this to:
+#1.  Read the value from the query string (e.g., ?a=5&b=10).
+#2. Automatically validate that it is an integer.
+#3. Convert it to an integer if possible.
+#b: int: Same logic for parameter b.
 
-#@app.post("/input"): Tells FastAPI to listen for POST requests (standard for sending data)
-#(a: int, b: int): Defines two input parameters, a and b, both expected to be integers
-#return{"result": a+b}: Returns the sum of a and b in a dictionary format, which FastAPI converts to JSON for the response
+#a + b: Performs the addition of the two integers.
+#{"result": ...}: Wraps the calculation in a dictionary to be returned as JSON.
+#uvicorn main:app --reload     main: The filename (without .py).  app: The variable name defined in the code (app = FastAPI()).
+#--reload: Automatically restarts the server when you change the code.
