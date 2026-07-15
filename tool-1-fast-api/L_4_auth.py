65906a1b35afaf986b020ce0b6d9d89388ea6720
Ares00700 <aresforwork007700@gmail.com>
2026-07-15 23:05:24 +0600

    L

---
+++ tool-1-fast-api/L_4_auth.py
@@ -0,0 +1,9 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto") #this will create a CryptContext object that uses bcrypt for hashing passwords
+
+hashed = pwd_context.hash("hello123")   #this will hash the password "hello123" using bcrypt    
+print(hashed)  # something like $2b$12$Kx....
+
+is_correct = pwd_context.verify("hello123", hashed)  #it verifies the password against the hash 
+print(is_correct)  # True
