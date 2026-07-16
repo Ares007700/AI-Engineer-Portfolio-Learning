e83662f2f7d64132f39b6015564a7714f66379c2
Ares00700 <aresforwork007700@gmail.com>
2026-07-17 00:52:14 +0600

    L

--- tool-1-fast-api/Set-up-database/models.py
+++ tool-1-fast-api/Set-up-database/models.py
@@ -7,3 +7,10 @@ class Item(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     description = Column(String, nullable=True)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
